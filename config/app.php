34d731a73cd378da343177c6aba7de3e7ffea05f
kirill <gluck1986@gmail.com>
2020-12-20 16:22:34 +0400

    phpunit auto dock settings and update

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Gluck1986\Support\AutoDoc\AutoDocServiceProvider::class,
+
     ],
 
     /*
---
+++ config/auto-doc.php
@@ -0,0 +1,105 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Route
+    |--------------------------------------------------------------------------
+    |
+    | Route which will return documentation
+    */
+
+    'route' => '/swagger',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Info block
+    |--------------------------------------------------------------------------
+    |
+    | Information fields
+    */
+
+    'info' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Documentation Template
+        |--------------------------------------------------------------------------
+        |
+        | You can use your custom documentation view
+        */
+
+        'description' => 'swagger-description',
+        'version' => '0.0.0',
+        'title' => 'Name of Your Application',
+        'termsOfService' => '',
+        'contact' => [
+            'email' => 'your@email.com'
+        ],
+        'license' => [
+            'name' => '',
+            'url' => ''
+        ]
+    ],
+    'swagger' => [
+        'version' => '2.0'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base API path
+    |--------------------------------------------------------------------------
+    |
+    | Base path for API routes. If config is set, all routes which starts from
+    | this value will be grouped.
+    */
+
+    'basePath' => '/',
+    'schemes' => [],
+    'definitions' => [],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Security Library
+    |--------------------------------------------------------------------------
+    |
+    | Library name, which used to secure the project.
+    | Available values: "jwt", "laravel", "null"
+    */
+
+    'security' => env('AUTO_DOK_SECURITY', null),
+    'defaults' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Default descriptions of code statuses
+        |--------------------------------------------------------------------------
+        */
+
+        'code-descriptions' => [
+            '200' => 'Operation successfully done',
+            '204' => 'Operation successfully done',
+            '404' => 'This entity not found'
+        ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Data Collector Class
+    |--------------------------------------------------------------------------
+    |
+    | Class of data collector, which will collect and save documentation
+    | It can be your own data collector class which should be inherited from
+    | Gluck1986\Support\AutoDoc\Interfaces\DataCollectorInterface interface,
+    | or our data collectors from next packages:
+    |
+    | ronasit/local-data-collector
+    | ronasit/laravel-remote-data-collector
+    |
+    | If config not set, will be using ronasit/local-data-collector
+    */
+
+    'data_collector' => '',
+    'production_path' => dirname(__DIR__) . DIRECTORY_SEPARATOR . env('LOCAL_DATA_COLLECTOR_PROD_PATH')
+];
